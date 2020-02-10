007d37eaca964fea166d0b70a1c8aa2be83bc237
82gnawf <82gnawf@users.noreply.github.com>
2020-02-10 10:13:08 -0800

    Added some classes that should be needed. (Task 4 Commit 1)

---
+++ src/main/ui/GamePanel.java
@@ -0,0 +1,7 @@
+package ui;
+
+// Represents a game having a boolean on whether the game is active, a player and a list of obstacles
+public class GamePanel {
+    private boolean activeGame;      // whether the game is active or not
+
+}
